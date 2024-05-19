d845487e9789d2d425bfb8b8dfa4b4bc383bd03c
Migu3e <itaymigul@gmail.com>
2024-05-19 15:35:05 +0300

    First Server Commit

---
+++ Server/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MyNamespace
+{
+    public class Server
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello");
+        }
+    
+    }
+}
