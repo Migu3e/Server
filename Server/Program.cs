ec0e13b0a69c6396139664dce9b106f655248310
Migu3e <itaymigul@gmail.com>
2024-05-23 16:08:32 +0300

    working server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,44 +1,59 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
-IPHostEntry ipEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
-
-// extracting local host ip (127.0.1.1)
-IPAddress ip = ipEntry.AddressList[0];
-
-// connect the server socket to client socket
-IPEndPoint ipEndPoint = new(ip,1234);
-
-using Socket server = new
-(
-    ipEndPoint.AddressFamily,
-    SocketType.Stream,
-    ProtocolType.Tcp
-);
-
-server.Bind(ipEndPoint);
-server.Listen();
-Console.WriteLine("server has started lisining on port 1234");
-
-var handler = await server.AcceptAsync();
-
-while (true)
+class Program
 {
-    var buffer = new byte[1_042];
-    
-    // received messege as a byte
-    var recived = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    static List<Socket> clients = new List<Socket>();
 
-    var mesageString = Encoding.UTF8.GetString(buffer, 0, recived);
-
-    if (mesageString != null)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Message from client: " + mesageString);
-        var response = "Message Recived!";
-        var responseByte = Encoding.UTF8.GetBytes(response);
+        IPHostEntry ipEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+        IPAddress ip = ipEntry.AddressList[0];
+        IPEndPoint ipEndPoint = new(ip, 1234);
 
-        await handler.SendAsync(responseByte, SocketFlags.None);
+        using Socket server = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        server.Bind(ipEndPoint);
+        server.Listen();
+        Console.WriteLine("Server has started listening on port 1234");
+
+        while (true)
+        {
+            Socket handler = await server.AcceptAsync();
+            clients.Add(handler);
+            _ = Task.Run(() => HandleClient(handler));
+        }
+    }
+
+    static async Task HandleClient(Socket handler)
+    {
+        while (true)
+        {
+            var buffer = new byte[1024];
+            var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            var receivedString = Encoding.UTF8.GetString(buffer, 0, received);
+            var parts = receivedString.Split('|');
+            var username = parts[0];
+            var message = parts[1];
+
+            if (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine($"Message from {username}: {message}");
+                
+                // Broadcast message to all clients except the sender
+                foreach (var client in clients)
+                {
+                    if (client != handler)
+                    {
+                        var response = $"{username}: {message}";
+                        var responseByte = Encoding.UTF8.GetBytes(response);
+                        await client.SendAsync(responseByte, SocketFlags.None);
+                    }
+                }
+            }
+        }
     }
 }
