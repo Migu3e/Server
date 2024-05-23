1499a0df80cc28144b600932714e747f5e1765a9
Migu3e <itaymigul@gmail.com>
2024-05-23 13:01:41 +0300

    first server

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,3 +10,35 @@ IPAddress ip = ipEntry.AddressList[0];
 
 // connect the server socket to client socket
 IPEndPoint ipEndPoint = new(ip,1234);
+
+using Socket server = new
+(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+);
+
+server.Bind(ipEndPoint);
+server.Listen();
+Console.WriteLine("server has started lisining on port 1234");
+
+var handler = await server.AcceptAsync();
+
+while (true)
+{
+    var buffer = new byte[1_042];
+    
+    // received messege as a byte
+    var recived = await handler.ReceiveAsync(buffer, SocketFlags.None);
+
+    var mesageString = Encoding.UTF8.GetString(buffer, 0, recived);
+
+    if (mesageString != null)
+    {
+        Console.WriteLine("Message from client: " + mesageString);
+        var response = "Message Recived!";
+        var responseByte = Encoding.UTF8.GetBytes(response);
+
+        await handler.SendAsync(responseByte, SocketFlags.None);
+    }
+}
