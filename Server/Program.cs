614f2fb31b05df7f1e2bb06ae7f3bfe2c0fba688
Migu3e <itaymigul@gmail.com>
2024-05-23 12:50:58 +0300

    first commit of featureserver

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,13 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-namespace MyNamespace
-{
-    public class Server
-    {
-        public static void Main()
-        {
-            Console.WriteLine("hello");
-        }
-    
-    }
-}
+IPHostEntry ipEntry = await Dns.GetHostEntryAsync(Dns.GetHostName());
+
+// extracting local host ip (127.0.1.1)
+IPAddress ip = ipEntry.AddressList[0];
+
+// connect the server socket to client socket
+IPEndPoint ipEndPoint = new(ip,1234);
