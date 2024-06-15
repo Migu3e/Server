52e346ea9abc47e651e14dd0395be0209686f556
Migu3e <itaymigul@gmail.com>
2024-06-15 18:15:02 +0300

    all rooms would need to be created with a password

--- Server/Models/Room.cs
+++ Server/Models/Room.cs
@@ -5,12 +5,22 @@ namespace Server.Models
     public class Room : IRoom
     {
         public string Name { get; set; }
+        public string Password { get; set; }
+
         public List<IClient> Members { get; private set; }
 
         public List<string> Messages { get; set; }
 
         public Room(string name)
         {
+            Password = "";
+            Name = name;
+            Members = new List<IClient>();
+            Messages = new List<string>();
+        }
+        public Room(string name, string password)
+        {
+            Password = password;
             Name = name;
             Members = new List<IClient>();
             Messages = new List<string>();
